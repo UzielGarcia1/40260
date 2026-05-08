f70702398fef739282cd406917fa977f6500a1c1
UzielGarcia1 <uzielito170@gmail.com>
2026-05-08 11:35:55 -0600

    Tarea Calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,13 +1,14 @@
 let resultado = document.getElementById("resultado")
 let botones = document.getElementsByTagName("button")
+
 for (const key in botones) {
     if (Object.prototype.hasOwnProperty.call(botones, key)) {
-        const boton = botones[key];
+        const boton = botones[key]
+
         if (boton.className != "operadores")
             boton.addEventListener("click", pintar)
         else
             boton.addEventListener("click", pintar2)
-        // console.log(boton)
     }
 }
 
@@ -18,17 +19,73 @@ function pintar(e) {
 
 function pintar2(e) {
     console.log(e.target.innerText)
+
     if (e.target.innerText == "+")
         suma()
+
+    if (e.target.innerText == "-")
+        resta()
+
+    if (e.target.innerText == "*")
+        multiplicacion()
+
+    if (e.target.innerText == "/")
+        division()
+
+    if (e.target.innerText == "=")
+        igual()
+
+    if (e.target.innerText == "Ce")
+        limpiar()
 }
 
 let prm1
-function suma(){
+let operador
+
+function suma() {
     prm1 = resultado.value
+    operador = "+"
+    resultado.value = ""
+}
+
+function resta() {
+    prm1 = resultado.value
+    operador = "-"
+    resultado.value = ""
+}
+
+function multiplicacion() {
+    prm1 = resultado.value
+    operador = "*"
+    resultado.value = ""
+}
+
+function division() {
+    prm1 = resultado.value
+    operador = "/"
     resultado.value = ""
 }
 
 function igual() {
-    prm2 = resultado.value
-    resultado.value = parseInt(prm1) + parseInt(prm2)
+
+    let prm2 = resultado.value
+
+    if (operador == "+")
+        resultado.value = parseFloat(prm1) + parseFloat(prm2)
+
+    if (operador == "-")
+        resultado.value = parseFloat(prm1) - parseFloat(prm2)
+
+    if (operador == "*")
+        resultado.value = parseFloat(prm1) * parseFloat(prm2)
+
+    if (operador == "/")
+        resultado.value = parseFloat(prm1) / parseFloat(prm2)
 }
+
+function limpiar() {
+    resultado.value = ""
+    prm1 = ""
+    operador = ""
+}
+
